3a3ac8b1a26f66e6a5e8bd928405649c9914a47d
Anthony Liu <anthonyliuhao@icloud.com>
2019-12-27 10:17:04 -0800

    Use the Codable type alias

--- Clima/Model/WeatherData.swift
+++ Clima/Model/WeatherData.swift
@@ -8,17 +8,18 @@
 
 import Foundation
 
-struct WeatherData: Decodable {
+// Using the Codable type alias (=Encodable + Decodable)
+struct WeatherData: Codable {
     let name: String
     let main: Main
     let weather: [Weather]
 }
 
-struct Main: Decodable {
+struct Main: Codable {
     let temp: Double
 }
 
-struct Weather: Decodable {
+struct Weather: Codable {
     let description: String
     let id: Int
 }
